6d815e01b747b7e2480e8c3e8dcb644381e05b31
Anatolie1 <gavriluta.anatolie@gmail.com>
2020-01-23 18:14:22 +0100

    firts commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Prototip_projet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clients Julie = new Clients("Julie", 1234);
+            Clients Jule = new Clients("Jule", 1234);
+
+            Julie.CreateAccount();
+            Julie.CreateAccount();
+
+            Jule.CreateAccount();
+            Jule.CreateAccount();
+
+
+        }
+    }
+
+}
---
+++ Transaction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototip_projet2
+{
+    static class Transaction
+    {
+        private static double _amount;
+        private static int _accountId;
+
+        public static void Withdraw(Accounts accounts, double amount)
+        {
+            Console.WriteLine("there are : " + accounts.GetAmount() + " of money from account " + accounts.GetAccountId());
+            double money = accounts.GetAmount() - amount;
+            accounts.SetAmount(money);
+            Console.WriteLine("there are : " + accounts.GetAmount() + " of money left");
+
+        }
+
+        public static void MoneyTransfer(Accounts DebitAccount, Accounts CreditAccount, double amount)
+        {
+            Console.WriteLine("Money on debit account before transfer: " + DebitAccount.GetAmount());
+            Console.WriteLine("Money on credit account before transfer: " + CreditAccount.GetAmount());
+            double money = DebitAccount.GetAmount() - amount;
+            DebitAccount.SetAmount(money);
+            CreditAccount.SetAmount(CreditAccount.GetAmount() + amount);
+            Console.WriteLine("Money on debit account after transfer: " + DebitAccount.GetAmount());
+            Console.WriteLine("Money on credit account after transfer: " + CreditAccount.GetAmount());
+
+        }
+    }
+}
